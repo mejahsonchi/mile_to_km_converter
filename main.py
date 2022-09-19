88fbeafca4df91edb06939462ffd64473a68038b
chinedu <mejahsonchi@gmail.com>
2022-09-19 22:14:53 +0100

    a little refining

--- main.py
+++ main.py
@@ -4,43 +4,43 @@ from tkinter import *
 window = Tk()
 window.title("Mile to Km Converter")
 window.minsize(width=60, height=70)
-window.config(padx=20,pady=20)
+window.config(padx=20, pady=20)
 
 
 # Entries
-entry = Entry(width=30)
+miles_input = Entry(width=30)
 # Add some text to begin with
-entry.insert(END, string="0")
+miles_input.insert(END, string="0")
 # Gets text in entry
-print(entry.get())
-entry.grid(column=1, row=0)
+#print(entry.get())
+miles_input.grid(column=1, row=0)
 
 
 # Labels
-label = Label(text="miles")
+miles_label = Label(text="miles")
 
-label.grid(column=2, row=0)
+miles_label.grid(column=2, row=0)
 
 # Labels
-label = Label(text="is equal to")
+equal_to_label = Label(text="is equal to")
 
-label.grid(column=0, row=1)
+equal_to_label.grid(column=0, row=1)
 
 # Labels
 show_label = Label(text="0")
 
 show_label.grid(column=1, row=1)
 
-label = Label(text="Km")
+km_label = Label(text="Km")
 
-label.grid(column=2, row=1)
+km_label.grid(column=2, row=1)
 # Buttons
 
 
 def calculate():
-    miles_int = int(entry.get())
-    km = round(miles_int * 1.6)
-    show_label.config(text=km)
+    miles_int = float(miles_input.get())
+    km = round(miles_int * 1.609)
+    show_label.config(text=f"{km}")
 
 
 # calls action() when pressed
